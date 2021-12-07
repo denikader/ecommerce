93660887908c2f5888fe12593332c8a77c31f3c9
deni Kader <92921224+denikader@users.noreply.github.com>
2021-12-07 20:10:20 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+const App = () => {
+  return <Home />;
+};
+
+export default App;
